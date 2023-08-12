b886876314b10c72b5735073b6448448411e5f8f
alexdamrow <alex.damrow@my.jcu.edu.au>
2023-08-12 16:42:19 +1000

    Add convert route

--- app.py
+++ app.py
@@ -17,5 +17,11 @@ def greet(name=""):
 def convert_celsius_to_fahrenheit(celsius):
     return (celsius * (9 / 5)) + 32
 
+
+@app.route('/f/<celsius>')
+def convert(celsius=0):
+    return f"{celsius} celsius = {convert_celsius_to_fahrenheit(float(celsius))} fahrenheit"
+
+
 if __name__ == '__main__':
     app.run()
